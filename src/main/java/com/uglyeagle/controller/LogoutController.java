d24fcfcd64713522608c83ad4cf46995c0be3811
Suraj-Mohapatra <suraj2895@gmail.com>
2025-08-19 22:27:15 +0530

    init

---
+++ src/main/java/com/uglyeagle/controller/LogoutController.java
@@ -0,0 +1,33 @@
+package com.uglyeagle.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.logout();
+        request.getSession(false).invalidate();
+        response.getWriter().write("logged out...");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.logout();
+        request.getSession(false).invalidate();
+        response.getWriter().write("logged out...");
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
