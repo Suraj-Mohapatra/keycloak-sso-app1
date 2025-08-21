1edbbd3cb98a7d7944e1cce16bf760769a64bb29
Suraj-Mohapatra <suraj2895@gmail.com>
2025-08-21 20:01:03 +0530

    quick

---
+++ src/main/java/com/uglyeagle/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.uglyeagle.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		
+	return "surjbhanu";
+	
+	}
+	
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "This is a public endpoint.";
+    }
+
+}
